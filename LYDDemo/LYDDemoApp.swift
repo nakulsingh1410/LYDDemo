216cf2a254cec540bbb37bb4ef820c5d512b66d7
Singh, Nakul <nakul.singh@capco.com>
2024-12-24 12:24:40 +0530

    Initial Commit

---
+++ LYDDemo/LYDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LYDDemoApp.swift
+//  LYDDemo
+//
+//  Created by Singh, Nakul on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LYDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
